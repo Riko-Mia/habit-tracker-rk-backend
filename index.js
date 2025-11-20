4d7fe38003c01d37828c4972c4b9811e9382f8f8
Riko Mia <rikomia722@gmail.com>
2025-11-20 13:10:06 +0600

    my habit update

--- index.js
+++ index.js
@@ -95,6 +95,7 @@ async function run() {
 
         app.patch("/habits/:id", async (req, res) =>{
             const id = req.params.id;
+            console.log(id)
             const query = {_id: new ObjectId(id)}
 
             const update = {
